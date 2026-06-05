cadf2b8c0589f8cfcb1c62cf57de57cf5c54b75a
lh <111371248+L-Har@users.noreply.github.com>
2026-06-05 09:57:18 -0500

    Add GeosConfig class, not used yet

---
+++ app/config/GeosConfig.py
@@ -0,0 +1,5 @@
+
+class GeosConfig:
+    VECTOR_EXTS = ["shp", "parquet", "pq", "gdb", "fgb", "fgdb"]
+    RASTER_EXTS = ["tiff", "tif", "geotif", "geotiff", "img", "mrsid", "jpg", "jpeg", "jpeg2000"]
+    POINT_CLOUD_EXTS = ["laz", "las", "copc"]
